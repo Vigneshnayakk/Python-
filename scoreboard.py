f476cef1eee17324f1cd339dc88f9dcf027e73c3
Vignesh Nayak <52487799+Vigneshnayakk@users.noreply.github.com>
2019-08-18 10:21:06 +0530

    Add files via upload

---
+++ scoreboard.py
@@ -0,0 +1,29 @@
+def climbingLeaderboard(scores, alice):
+    l=[];r=[]
+    for i in scores:
+        if i  not in l:
+            l.append(i)
+    l.sort(reverse=True)
+    for v in alice:
+        c= l + [v]
+        c.sort(reverse=True)
+        temp=c.index(v)
+        r.append((temp+1))
+        c.pop(temp)
+    return r
+
+
+
+
+
+
+
+
+
+
+scores_count = int(input())
+scores = list(map(int, input().rstrip().split()))
+alice_count = int(input())
+alice = list(map(int, input().rstrip().split()))
+result = climbingLeaderboard(scores, alice)
+print(result)
